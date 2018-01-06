bc17d30d49fe2a534145350d1075e55c05b3810f
chuanm <chuanm@rpi.edu>
2018-01-06 18:11:19 -0500

    Skill now behaves properly on saying "no" or "I don't know" in-game. Next issue to fix is prevent the skill from closing if a wrong answer is provided

--- CountryQuiz.py
+++ CountryQuiz.py
@@ -56,8 +56,27 @@ def initGame():
 @ask.intent("DontStartGame")
 
 def dontStartGame():
+    return statement("Ok, see you next time")
+
+#If user indicates they do not know while in game, count the question wrong, otherwise exit skill
+@ask.intent("SayNo")
+
+def sayNo():
+    global game_started
+    global list_pos
+    global questions_correct
+    global countries_dict
+    global question_order
+    global number_questions
+
+    print game_started
+
     if not game_started:
         return statement("Ok, see you next time")
+    elif list_pos >= number_questions:
+        return statement("Sorry, that is not correct. The correct answer was %s. Out of %s questions you got %s correct. Thanks for playing" % (countries_dict[question_order[list_pos-1]][1][0],str(number_questions),str(questions_correct))).simple_card(title="Final Score",content="%d correct out of %d" % (questions_correct,number_questions))
+    else:
+        return question("Sorry, that is not correct. The correct answer was %s. Now, can you guess what country this flag belongs to?" % (countries_dict[question_order[list_pos-1]][1][0])).standard_card(title="Which Country?",small_image_url=countries_dict[question_order[list_pos]][0],large_image_url=countries_dict[question_order[list_pos]][0])
 
 #If user provides answer, check to see if it is correct, then either end the game if number_questions have been asked
 #Or provide the next question
@@ -72,8 +91,6 @@ def checkAnswer(country_name):
     if game_started:
         country_name = country_name.lower() #Sanitize input to lowercase
         correct_name = countries_dict[question_order[list_pos]][1]
-        print country_name
-        print correct_name
         list_pos += 1
         #If the allotted number of questions have been asked
         if list_pos >= number_questions:
