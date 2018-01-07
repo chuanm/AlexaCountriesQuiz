1ef7cf7fe6e52dc2e3b5aaaead45f7096f9aa477
chuanm <chuanm@rpi.edu>
2018-01-06 22:11:11 -0500

    Fixed crash when invalid response given during game. All major bugs appear to be fixed

--- CountryQuiz.py
+++ CountryQuiz.py
@@ -19,6 +19,9 @@ list_pos = 0 #Keeps track of how far down the question list we've gone
 @ask.launch
 
 def newGame():
+
+    print "1"
+
     global game_started
     game_started = False
     welcome_msg = render_template('welcome')
@@ -29,6 +32,9 @@ def newGame():
 @ask.intent("StartGame")
 
 def initGame():
+
+    print "2"
+
     global game_started
     global questions_correct
     global countries_dict
@@ -51,17 +57,24 @@ def initGame():
         #Get the flag image of the first country in question_order and ask user with flag displayed on card
         what_country = render_template('which_country')
         return question(what_country).standard_card(title="Which Country?",small_image_url=countries_dict[question_order[list_pos]][0],large_image_url=countries_dict[question_order[list_pos]][0])
-
+    else:
+        return question("That's not a country, please name a country, or say I don't know")
 #If user indicates they do not want to start a new game, exit
 @ask.intent("DontStartGame")
 
 def dontStartGame():
+
+    print "3"
+
     return statement("Ok, see you next time")
 
 #If user indicates they do not know while in game, count the question wrong, otherwise exit skill
 @ask.intent("SayNo")
 
 def sayNo():
+
+    print "4"
+
     global game_started
     global list_pos
     global questions_correct
@@ -69,42 +82,12 @@ def sayNo():
     global question_order
     global number_questions
 
-    print game_started
-
     if not game_started:
         return statement("Ok, see you next time")
     elif list_pos >= number_questions:
         return statement("Sorry, that is not correct. The correct answer was %s. Out of %s questions you got %s correct. Thanks for playing" % (countries_dict[question_order[list_pos-1]][1][0],str(number_questions),str(questions_correct))).simple_card(title="Final Score",content="%d correct out of %d" % (questions_correct,number_questions))
     else:
         return question("Sorry, that is not correct. The correct answer was %s. Now, can you guess what country this flag belongs to?" % (countries_dict[question_order[list_pos-1]][1][0])).standard_card(title="Which Country?",small_image_url=countries_dict[question_order[list_pos]][0],large_image_url=countries_dict[question_order[list_pos]][0])
-
-#If user provides answer, check to see if it is correct, then either end the game if number_questions have been asked
-#Or provide the next question
-@ask.intent("ProvideAnswer", convert={'country_name':str})        
-
-def checkAnswer(country_name):
-    global list_pos
-    global questions_correct
-    global countries_dict
-    global question_order
-    global number_questions
-    if game_started:
-        country_name = country_name.lower() #Sanitize input to lowercase
-        correct_name = countries_dict[question_order[list_pos]][1]
-        list_pos += 1
-        #If the allotted number of questions have been asked
-        if list_pos >= number_questions:
-            if country_name in correct_name:
-                questions_correct += 1
-                return statement("Yes that is correct. Here is a fun fact about %s: %s. Out of %s questions you got %s correct. Thanks for playing" % (countries_dict[question_order[list_pos-1]][1][0],countries_dict[question_order[list_pos-1]][2],str(number_questions),str(questions_correct))).simple_card(title="Final Score",content="%d correct out of %d" % (questions_correct,number_questions))
-            else:
-                return statement("Sorry, that is not correct. The correct answer was %s. Out of %s questions you got %s correct. Thanks for playing" % (countries_dict[question_order[list_pos-1]][1][0],str(number_questions),str(questions_correct))).simple_card(title="Final Score",content="%d correct out of %d" % (questions_correct,number_questions))
-        #Otherwise, get info about the next question to ask and ask it
-        if country_name in correct_name:
-            questions_correct += 1
-            return question("Yes that is correct. Here is a fun fact about %s: %s. Now, can you guess what country this flag belongs to?" % (countries_dict[question_order[list_pos-1]][1][0],countries_dict[question_order[list_pos-1]][2])).standard_card(title="Which Country?",small_image_url=countries_dict[question_order[list_pos]][0],large_image_url=countries_dict[question_order[list_pos]][0])
-        else:
-             return question("Sorry, that is not correct. The correct answer was %s. Now, can you guess what country this flag belongs to?" % (countries_dict[question_order[list_pos-1]][1][0])).standard_card(title="Which Country?",small_image_url=countries_dict[question_order[list_pos]][0],large_image_url=countries_dict[question_order[list_pos]][0])
 
 if __name__ == '__main__':
     app.run(debug=True)
